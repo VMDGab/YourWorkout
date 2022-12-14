d7f5c3903ba48f8f78cce842e65cdb67351e8347
Gabriel <gmatosdantas777@gmail.com>
2022-12-13 22:50:00 -0300

    Desenvolvendo as telas do app

---
+++ App.tsx
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import {ThemeProvider} from 'styled-components'
+import { Routes } from './src/routes/app.routes';
+
+
+import { useFonts, Prompt_600SemiBold, Prompt_500Medium, Prompt_700Bold } from '@expo-google-fonts/prompt';
+
+//--------------------------------------------------
+
+import Theme from './src/global/styles/Theme';
+import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+//---------------------------------------------------
+
+
+
+
+export default function App() {
+
+
+  const [FontsLoaded] = useFonts({
+
+    Prompt_600SemiBold, 
+    Prompt_500Medium, 
+    Prompt_700Bold
+    
+  })
+  
+  if (!FontsLoaded){
+    return <AppLoading />
+  }
+  
+  return (
+  
+    <ThemeProvider theme={Theme} >
+       <NavigationContainer>
+         <Routes/>
+        </NavigationContainer>
+       </ThemeProvider>
+ 
+  );
+}
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons , Octicons , AntDesign  } from '@expo/vector-icons'
+import { Dashboard } from "../pages/Dashboard";
+import { Profile } from "../pages/Profile";
+import { Workouts} from "../pages/Workouts";
+import { Platform } from 'react-native'
+import Theme from "../global/styles/Theme";
+const Tab = createBottomTabNavigator();
+
+export function Routes(){
+
+  const theme = Theme
+  
+    return(
+
+        <Tab.Navigator initialRouteName="Dashboard" 
+        screenOptions={{
+            tabBarActiveTintColor: theme.colors.Primary,
+            tabBarInactiveTintColor: theme.colors.Title,
+            tabBarLabelPosition: 'below-icon',
+            headerShown: false,
+            tabBarStyle: {
+              paddingVertical: Platform.OS === 'ios' ? 20 : 13,
+              height: 70,
+              paddingBottom:13,
+              borderTopColor: theme.colors.Primary,
+              borderTopWidth:3,
+            },
+            tabBarLabelStyle: {
+              fontFamily: theme.Fonts.Medium
+            }
+          }}
+        >
+            <Tab.Screen 
+            options={{
+          tabBarIcon: ({ size, color }) => (
+            <Octicons 
+              name={'person'}
+              size={size}
+              color={color}
+            />
+          )
+        }}
+        name={'Perfil'}
+        component={Profile}
+      />
+
+        <Tab.Screen 
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <AntDesign name={'home'} size={size} color={color} />
+          )
+        }}
+        name={'Inicio'}
+        component={Dashboard}
+      />
+      <Tab.Screen 
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons  name={'arm-flex-outline'} size={size} color={color} />
+          )
+        }}
+        name={'Seus Treinos'}
+        component={Workouts}
+      />
+
+        </Tab.Navigator>
+
+
+    )
+
+}
