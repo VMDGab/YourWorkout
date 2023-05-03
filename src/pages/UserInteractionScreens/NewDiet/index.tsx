8844c0d44292715edf070af840b82849cbdf63e5
Gabriel <gmatosdantas777@gmail.com>
2023-05-03 04:56:42 -0300

    Desenvolvendo tela de criação de dieta

--- src/pages/UserInteractionScreens/NewDiet/index.tsx
+++ src/pages/UserInteractionScreens/NewDiet/index.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
-import { 
+import EclipseSvg from '../../../assets/Ellipse.svg'
+import {
     Container,
- } from './styles';
+    DietOverview,
+    Calorie,
+    CalorieValue,
+    Type,
+    MacroValue,
+    Value,
+    OtherMacros,
+    Emphasis,
+    ButtonStyle,
+} from './styles';
+import { ContentButton } from '../../../components/ContentButton';
 
 export function NewDiet() {
-  return (
-    <Container>
+    return (
+        <Container>
+            <DietOverview>
+                <Emphasis>
+                    <EclipseSvg />
 
-    </Container>
-  );
+                    <Calorie>
+                        <CalorieValue>3580</CalorieValue>
+                        <Type>Calorias</Type>
+                    </Calorie>
+                </Emphasis>
+                <OtherMacros>
+
+                    <MacroValue>
+                        <Value>160g</Value>
+                    </MacroValue>
+                    <Type>Proteínas</Type>
+
+                    <MacroValue>
+                        <Value>250g</Value>
+                    </MacroValue>
+                    <Type>Carboidratos</Type>
+
+                    <MacroValue>
+                        <Value>60g</Value>
+                    </MacroValue>
+                    <Type>Gorduras</Type>
+
+                </OtherMacros>
+            </DietOverview>
+            <ButtonStyle>
+            <ContentButton title='Nova refeição' icon='plus'/>
+            </ButtonStyle>
+        </Container>
+    );
 }
--- src/pages/UserInteractionScreens/NewDiet/styles.ts
+++ src/pages/UserInteractionScreens/NewDiet/styles.ts
@@ -2,4 +2,64 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({theme}) => theme.colors.BACKGROUND};
 `;
+
+export const DietOverview = styled.View`
+ background-color: ${({theme}) => theme.colors.Shape};
+
+ width: 350px;
+height: 200px;
+
+flex-direction: row;
+ align-self: center;
+
+ margin-top: 100px;
+ padding: 24px;
+ 
+ border-radius: 25px;
+ border-width: 2px;
+ border-color: ${({theme}) => theme.colors.border};
+ justify-content: space-between;
+`
+export const Calorie = styled.View`
+align-items: center;
+margin-top: -100px;
+`
+export const CalorieValue = styled.Text`
+font-size: 32px;
+font-family: ${({theme}) => theme.Fonts.SemiBold};
+color: ${({theme}) => theme.colors.Primary};
+
+`
+export const Type = styled.Text`
+font-size: 13px;
+font-family: ${({theme}) => theme.Fonts.SemiBold};
+color: ${({theme}) => theme.colors.Title};
+
+`
+export const MacroValue = styled.View`
+width: 90px;
+
+border-bottom-width: 2px;
+border-color: ${({theme}) => theme.colors.Primary};
+
+align-items: center;
+`
+export const Value = styled.Text`
+font-size: 18px;
+font-family: ${({theme}) => theme.Fonts.Bold};
+color: ${({theme}) => theme.colors.Title};
+`
+export const OtherMacros = styled.View`
+align-items: center;
+`
+
+export const Emphasis = styled.View`
+margin-top: 25px;
+`
+export const ButtonStyle = styled.View`
+align-items: center;
+margin-top: 25px;
+
+`
